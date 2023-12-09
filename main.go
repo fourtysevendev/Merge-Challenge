ffd6626ad1bcb00612209915f9809c5876b3262b
fourtysevendev <fourtysevendev@protonmail.com>
2023-12-09 13:51:12 +0100

    feat(merge Intervalls): sort and return of a list of intervalls

--- main.go
+++ main.go
@@ -1,5 +1,43 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+)
 
+// Interval has a start and end point
+type Interval struct {
+	Start int
+	End   int
+}
+
+func mergeIntervals(intervals []Interval) []Interval {
+	// if the list of intervals is empty or has length 1
+	// then return the list of intervals
+	if len(intervals) <= 1 {
+		return intervals
+	}
+
+	// sort the intervals by start point
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i].Start < intervals[j].Start
+	})
+	fmt.Println(intervals)
+	return intervals
+
+}
+
+func main() {
+	intervals := []Interval{
+		{1, 5},
+		{2, 8},
+		{6, 9},
+		{10, 15},
+		{13, 18},
+		{20, 40},
+	}
+
+	overlappingIntervals := mergeIntervals(intervals)
+
+	fmt.Println(overlappingIntervals)
 }
